b01d23f09254f391e0ac5c3fe06a9c5e614e8910
Ade Attwood <ade@gl2tech.com>
2025-06-29 07:30:20 +0100

    fix: return error with file does not exist
    Summary:
    
    When the the agent tries to read a file that does not exist, the thing crashes.
    Now we are returning and error to tell the agent that the file does not exist.
    
    Test Plan:
    
    CI and unit tests.

--- tools/read.ts
+++ tools/read.ts
@@ -7,6 +7,19 @@ export default tool({
     fileName: z.string().describe("The filename of the file you want ot read"),
   }),
   execute: async ({ fileName }) => {
-    return await Deno.readTextFile(fileName);
+    try {
+      const stats = await Deno.lstat(fileName);
+      if (stats.isFile) {
+        return await Deno.readTextFile(fileName);
+      }
+
+      return `ERROR: '${fileName}' must be a file, directory found`;
+    } catch (err) {
+      if (!(err instanceof Deno.errors.NotFound)) {
+        throw err;
+      }
+
+      return "ERORR: file not found";
+    }
   },
 });
