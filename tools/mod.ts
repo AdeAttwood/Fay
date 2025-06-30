50e1701bd5fa56cc5468fb66fb1da10475baea61
Ade Attwood <ade@gl2tech.com>
2025-06-30 18:02:08 +0100

    chore: add the edit and glob tools to the exports
    Summary:
    
    The glob and edit tools are not currently being exported for the agent to see
    them. This adds them so we can start using them.
    
    Test Plan:
    
    N / A

--- tools/mod.ts
+++ tools/mod.ts
@@ -2,10 +2,14 @@ import ghPullReviewReviews from "./gh_pull_request_reviews.ts";
 import read from "./read.ts";
 import run from "./run.ts";
 import write from "./write.ts";
+import edit from "./edit.ts";
+import glob from "./glob.ts";
 
 export default {
   read,
   write,
   run,
   ghPullReviewReviews,
+  edit,
+  glob,
 };
