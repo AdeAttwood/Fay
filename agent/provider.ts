b7e4112eecb8685a2fcaec0888f3aba32582c65e
Ade Attwood <ade@gl2tech.com>
2025-09-19 17:54:30 +0100

    refactor: make the models and providers come from a config

--- agent/provider.ts
+++ agent/provider.ts
@@ -2,25 +2,46 @@ import { google } from "@ai-sdk/google";
 import { anthropic } from "@ai-sdk/anthropic";
 import type { Configuration } from "./config.ts";
 
-export function createProvider(modelName: string) {
-  switch (modelName) {
-    case "gemini-default":
-      return google("gemini-2.0-flash");
-    case "gemini-2.5-pro":
-    case "gemini-2.0-flash":
-      return google(modelName);
+// http get https://models.dev/api.json | values | each { |provider|
+//   $provider.models | values | each { |model|
+//     {
+//       id: $"($provider.id)/($model.id)",
+//       provider: $provider.id,
+//       model: $model.id,
+//       display: $"($provider.name) / ($model.name)",
+//       cost: ($model | get -i cost | default {}),
+//     }
+//   }
+// }
+// | flatten
+// | where provider == "google" or provider == "opencode" or provider == "anthropic"
+// | to json
+// | save -f ./agent/providers.json
+import providersConfig from './providers.json'  with { type: "json" };
 
-    case "claude-default":
-      return anthropic("claude-sonnet-4-20250514");
-    case "claude-3-sonnet-20240229":
-    case "claude-3-5-sonnet-20241022":
-    case "claude-3-7-sonnet-20250219":
-    case "claude-sonnet-4-20250514":
-    case "claude-opus-4-20250514":
-      return anthropic(modelName);
+const ALIASES: Record<string, string> = {
+  "google-default": "google/gemini-2.0-flash",
+  "claude-default": "anthropic/claude-sonnet-4-20250514",
+}
+
+export const providers = providersConfig;
+
+export function createProvider(modelName: string) {
+  const model = modelName in ALIASES ? ALIASES[modelName] : modelName;
+  const config = providersConfig.find(p => p.id == model);
+  if (!config) {
+    throw new Error(`Unable to infer model '${modelName}'`);
+  }
+
+  switch (config.provider) {
+    case "google":
+      return google(config.model);
+
+    case "anthropic":
+      return anthropic(config.model);
 
     default:
-      throw new Error(`Unable to infer model '${modelName}'`);
+      throw new Error(`Model '${modelName}' is not configured correctly.`);
   }
 }
 
