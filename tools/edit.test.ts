5a5a8714d1ed0d7aa0414bfcdccb5279034ee3cb
Ade Attwood <ade@gl2tech.com>
2025-12-23 16:51:28 +0000

    fix: handle line endings better
    Summary:
    
    When there are windows line endings in a file, the edit tool fails to add the
    content.
    
    Now when we are editing we are normalizing  all the content to unix line
    endings before trying to replace the content.
    
    Test Plan:
    
    Unit tests.

--- tools/edit.test.ts
+++ tools/edit.test.ts
@@ -20,3 +20,63 @@ Deno.test("edit tool", async () => {
 
   await Deno.remove(testFile);
 });
+
+Deno.test("edit tool with unix line endings", async () => {
+  const testFile = await Deno.makeTempFile();
+  await Deno.writeTextFile(testFile, "hello world\r\nSome content");
+
+  const result = await edit.execute?.(
+    {
+      fileName: testFile,
+      oldContent: "hello world\nSome",
+      newContent: "my\nnew",
+    },
+    { toolCallId: "edit", messages: [] },
+  );
+
+  assertEquals(result, `Successfully edited ${testFile}`);
+  const newContent = await Deno.readTextFile(testFile);
+  assertEquals(newContent, "my\nnew content");
+
+  await Deno.remove(testFile);
+});
+
+Deno.test("edit tool with mixed line endings", async () => {
+  const testFile = await Deno.makeTempFile();
+  await Deno.writeTextFile(testFile, "hello world\r\nSome content");
+
+  const result = await edit.execute?.(
+    {
+      fileName: testFile,
+      oldContent: "hello world\r\nSome",
+      newContent: "my\nnew",
+    },
+    { toolCallId: "edit", messages: [] },
+  );
+
+  assertEquals(result, `Successfully edited ${testFile}`);
+  const newContent = await Deno.readTextFile(testFile);
+  assertEquals(newContent, "my\nnew content");
+
+  await Deno.remove(testFile);
+});
+
+Deno.test("edit tool with mixed line endings in the old content", async () => {
+  const testFile = await Deno.makeTempFile();
+  await Deno.writeTextFile(testFile, "hello world\nSome content");
+
+  const result = await edit.execute?.(
+    {
+      fileName: testFile,
+      oldContent: "hello world\r\nSome",
+      newContent: "my\nnew",
+    },
+    { toolCallId: "edit", messages: [] },
+  );
+
+  assertEquals(result, `Successfully edited ${testFile}`);
+  const newContent = await Deno.readTextFile(testFile);
+  assertEquals(newContent, "my\nnew content");
+
+  await Deno.remove(testFile);
+});
